6e47e3cbb1c8d13ec8c5291f8123b49f5e70e754
Alexander Danson <alex.danson2@gmail.com>
2020-03-24 21:49:34 +0100

    testing accessing api and switching lights off and on

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+from qhue import Bridge, create_new_username
+import time
+
+username = create_new_username("192.168.1.204")
+
+b = Bridge("192.168.1.204", username)
+
+print(b.url)
+lights = b.lights
+print(lights.url)
+
+
+for i in range(5):
+    info = lights[1]()
+    status = info['state'].get('on')
+    print(status)
+    if status is True:
+        lights[1].state(on=False)
+        print("Lights off")
+        time.sleep(3)
+    else:
+        lights[1].state(on=True)
+        print("Lights on")
+        time.sleep(3)
+
+
 
